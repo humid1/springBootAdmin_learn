23f6f63b8efc429bf7d5c2bc860527baf2d75dc8
qiujianping <qiujianp123@163.com>
2021-03-19 16:05:03 +0800

    springboot-Admin的案例

---
+++ README.md
@@ -0,0 +1,5 @@
+# eureka
+访问地址：http://localhost:8761
+
+# spring-boot-admin
+控制台访问地址：http://localhost:8769
---
+++ boot-client/src/test/java/com/example/bootclient/BootClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bootclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
